07655fc8bbd6e1694795c055c24db0266c6cc923
Amarthyanath B <amarthyanathb@Amar-MacPro.local>
2025-02-25 10:56:16 +0530

    Initial commit

---
+++ trip_planner/trip_agents.py
@@ -0,0 +1,53 @@
+from crewai import Agent
+from langchain_community.llms import Ollama
+
+from tools.browser_tools import BrowserTools
+from tools.calculator_tools import CalculatorTools
+from tools.search_tools import SearchTools
+llm = Ollama(
+    model="llama3.3:70b",
+    base_url="http://localhost:11434",
+)
+
+class TripAgents():
+
+  def city_selection_agent(self):
+    return Agent(
+        role='City Selection Expert',
+        goal='Select the best city based on weather, season, and prices',
+        backstory=
+        'An expert in analyzing travel data to pick ideal destinations',
+        tools=[
+            SearchTools.search_internet,
+            BrowserTools.scrape_and_summarize_website,
+        ],
+        # llm=llm,
+        verbose=True)
+
+  def local_expert(self):
+    return Agent(
+        role='Local Expert at this city',
+        goal='Provide the BEST insights about the selected city',
+        backstory="""A knowledgeable local guide with extensive information
+        about the city, it's attractions and customs""",
+        tools=[
+            SearchTools.search_internet,
+            BrowserTools.scrape_and_summarize_website,
+        ],
+        # llm=llm,
+        verbose=True)
+
+  def travel_concierge(self):
+    return Agent(
+        role='Amazing Travel Concierge',
+        goal="""Create the most amazing travel itineraries with budget and 
+        packing suggestions for the city""",
+        backstory="""Specialist in travel planning and logistics with 
+        decades of experience""",
+        tools=[
+            SearchTools.search_internet,
+            BrowserTools.scrape_and_summarize_website,
+            CalculatorTools.calculate,
+        ],
+        # llm=llm,
+        verbose=True)
